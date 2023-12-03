487af8798e5d85a5078e32a1f8b038b1888fd01b
sanarWH <vkhv0702@gmail.com>
2023-12-03 15:39:34 +0300

    master

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,19 +1,3 @@
-import java.io.File
-
 fun main() {
 
-    val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.appendText("hello привет \n")
-    wordsFile.appendText("dog собака")
-    wordsFile.appendText("cat кошка")
-
-    wordsFile.readLines()
-    println(wordsFile.readLines())
-
-    var wordCount = 2
-
-    while (wordsFile[wordCount] < wordCount) {
-        println(wordsFile.readLines()[wordCount])
-    }
 }
