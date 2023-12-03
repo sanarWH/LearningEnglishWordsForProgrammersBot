38dd12b930fbe423807dd439cb7e4af406800b54
sanarWH <vkhv0702@gmail.com>
2023-12-03 17:21:27 +0300

    task01

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,16 +4,14 @@ fun main() {
 
     val wordsFile: File = File("words.txt")
     wordsFile.createNewFile()
+    wordsFile.writeText("") //это строка для того, чтобы при каждов новом запуске функции мэйн очищать файл
     wordsFile.appendText("hello привет \n")
-    wordsFile.appendText("dog собака")
-    wordsFile.appendText("cat кошка")
+    wordsFile.appendText("dog собака\n")
+    wordsFile.appendText("cat кошка\n")
 
-    wordsFile.readLines()
-    println(wordsFile.readLines())
+    val readWorldsFile = wordsFile.readLines()
 
-    var wordCount = 2
-
-    while (wordsFile[wordCount] < wordCount) {
-        println(wordsFile.readLines()[wordCount])
+    for (i in readWorldsFile) {
+        println(i)
     }
 }
