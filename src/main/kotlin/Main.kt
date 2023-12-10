d450234493227156e1bbd44bce4265e4942d927a
sanarWH <vkhv0702@gmail.com>
2023-12-10 18:26:17 +0300

    task02 solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,14 +4,26 @@ fun main() {
 
     val wordsFile: File = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("") //это строка для того, чтобы при каждов новом запуске функции мэйн очищать файл
-    wordsFile.appendText("hello привет \n")
-    wordsFile.appendText("dog собака\n")
-    wordsFile.appendText("cat кошка\n")
+    wordsFile.writeText("") //это строка для того, чтобы при каждом новом запуске функции мэйн очищать файл
+    wordsFile.appendText("hello|привет\n")
+    wordsFile.appendText("dog|собака\n")
+    wordsFile.appendText("cat|кошка\n")
 
     val readWorldsFile = wordsFile.readLines()
 
+    val listOfDictionary: MutableList<Word> = mutableListOf()
+    val counterOfLearnedWords = listOfDictionary.size
+
     for (i in readWorldsFile) {
-        println(i)
+        val split = i.split("|")
+        listOfDictionary.add(Word(split[0], split[1]))
     }
+
+    println(listOfDictionary)
+    println("Количество выученных слов: $counterOfLearnedWords")
 }
+
+data class Word (
+    val original: String,
+    val translate: String,
+)
